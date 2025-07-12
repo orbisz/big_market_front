593a8c565a2c5fdfdbf84069d13cc7592caea3cb
zxy <zxy0613zxy@outlook.com>
2025-07-12 19:36:32 +0800

    第一阶段抽奖部署

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  /* config options here */
-    env:{
-        API_HOST_URL:process.env.API_HOST_URL
-    }
-};
-
-export default nextConfig;
